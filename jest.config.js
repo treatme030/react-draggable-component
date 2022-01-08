79525c5e89c25c321275d1260832a254d392105a
treatme030 <treatme8192@gmail.com>
2022-01-08 23:00:40 +0900

    feat : 타입스크립트 테스트 코드 작성을 위한 환경설정

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/$1'
+  },
+  testMatch: [
+      '<rootDir>/**/*.test.(js|jsx|ts|tsx)', '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
+};
